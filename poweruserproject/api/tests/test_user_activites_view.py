35df363da5132c001257fa16fe3254f550607e6f
Alyson Brito <alysongirao@gmail.com>
2021-06-21 11:27:32 -0300

    Adicionado endpoint para contar a quantidade de usuarios em um dia

--- poweruserproject/api/tests/test_user_activites_view.py
+++ poweruserproject/api/tests/test_user_activites_view.py
@@ -1,46 +1,7 @@
-import datetime
-
 import pytest
 
 from poweruserproject.api.models import UserActivity
 from poweruserproject.api.serializers import UserActivitySerializer
-
-
-@pytest.fixture
-def user_activities(db):
-    activites = [
-        UserActivity(user_id=1,
-                     date=datetime.date(2021, 2, 1),
-                     used=True),
-        UserActivity(user_id=1,
-                     date=datetime.date(2021, 2, 2),
-                     used=True),
-        UserActivity(user_id=1,
-                     date=datetime.date(2021, 2, 3),
-                     used=False),
-        UserActivity(user_id=1,
-                     date=datetime.date(2021, 2, 4),
-                     used=True),
-        UserActivity(user_id=1,
-                     date=datetime.date(2021, 5, 1),
-                     used=True),
-        UserActivity(user_id=1,
-                     date=datetime.date(2021, 5, 2),
-                     used=False),
-        UserActivity(user_id=1,
-                     date=datetime.date(2021, 5, 3),
-                     used=True),
-        UserActivity(user_id=2,
-                     date=datetime.date(2021, 2, 1),
-                     used=True),
-        UserActivity(user_id=2,
-                     date=datetime.date(2021, 5, 1),
-                     used=True),
-        UserActivity(user_id=2,
-                     date=datetime.date(2021, 5, 2),
-                     used=False)
-    ]
-    return UserActivity.objects.bulk_create(activites)
 
 
 @pytest.fixture
---
+++ poweruserproject/api/tests/test_user_count_view.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture
+def response_users_count(db, client, user_activities):
+    return client.get('/api/users/activities/?day=2021-05-02')
+
+
+def test_status_code(response_users_count):
+    assert response_users_count.status_code == 200
+
+
+def test_response_body(response_users_count):
+    assert {'quantity': 1} == response_users_count.data

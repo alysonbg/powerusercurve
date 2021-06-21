79778af8b22ce5ed66671bcee1b967c160ae4c60
Alyson Brito <alysongirao@gmail.com>
2021-06-21 11:27:32 -0300

    Adicionado endpoint para contar a quantidade de usuarios em um dia

--- poweruserproject/api/urls.py
+++ poweruserproject/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from poweruserproject.api.views import list_user_activities
+from poweruserproject.api.views import list_user_activities, list_amount_of_users_for_a_day
 
 urlpatterns = [
     path('users/<int:user_id>/', list_user_activities),
+    path('users/activities/', list_amount_of_users_for_a_day),
 ]
--- poweruserproject/api/views.py
+++ poweruserproject/api/views.py
@@ -15,3 +15,10 @@ def list_user_activities(request, user_id):
     serializer = UserActivitySerializer(activities, many=True)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def list_amount_of_users_for_a_day(request):
+    day = request.query_params.get('day')
+    users_count = UserActivity.objects.filter(date=day, used=True).count()
+    return Response({'quantity': users_count}, status=status.HTTP_200_OK)
